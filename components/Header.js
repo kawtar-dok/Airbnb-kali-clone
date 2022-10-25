eede4ca3474d76f2b1027c36885929ce33e54f4a
kawtar itoujane <kawtaritoujane@gmail.com>
2022-10-26 00:21:17 +0100

    add the dark mode

--- components/Header.js
+++ components/Header.js
@@ -75,14 +75,15 @@ function Header() {
      {/* Right */}
      <div className='flex items-center justify-end space-x-4
      text-gray-500'>
-        {/* Dark Mode  */}
-        <div className='flex items-center space-x-10 cursor-pointer'>
-        {renderThemeChange()}
-         </div>
+        
        <p className='hidden md:inline-flex cursor-pointer'>
         Become a Host
        </p>
        <GlobeAltIcon className='h-6 cursor-pointer'/>
+       {/* Dark Mode  */}
+       <div className='flex items-center space-x-2 cursor-pointer'>
+        {renderThemeChange()}
+         </div>
       <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
          <MenuIcon className='h-6'/>
          <UserCircleIcon  className='h-6'/>
