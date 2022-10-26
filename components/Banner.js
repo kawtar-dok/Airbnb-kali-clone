673a6595ece783cfd504f58e97eae24c3ee0fd64
kawtar itoujane <kawtaritoujane@gmail.com>
2022-10-26 14:11:01 +0100

    add the search compoenets

--- components/Banner.js
+++ components/Banner.js
@@ -10,15 +10,24 @@ function Banner() {
         layout='fill'
         objectFit='cover'
       />
-      <div className='absolute top-1/2 w-full text-center '>
+      
+     
+      <div className='absolute top-0 w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] 
+    dark:text-gray-900
+     
+      dark:bg-gradient-radial to-black from-transparent'
+      >
+      {/*  dark:bg-gradient-to-t from-black via-transparent to-black*/}
+      <div className='relative top-1/2 text-center '>
          <p className='text-sm sm:text-lg '>
             Not sure where to go? Perfect.
          </p>
-         <button className='text-purple-500 bg-white px-4 py-2 sm:px-10 sm:py-4 shadow-md rounded-full 
+         <button className='text-purple-500 bg-white dark:bg-gray-200/80 px-4 py-2 sm:px-10 sm:py-4 shadow-md rounded-full 
          font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150
          '>
             I'm flexible
          </button>
+         </div>
       </div>
     </div>
   )
--- components/Header.js
+++ components/Header.js
@@ -12,11 +12,37 @@ import {
    } from "@heroicons/react/solid"
 import {SunIcon ,MoonIcon} from "@heroicons/react/solid";
 import {useTheme} from "next-themes";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+
 
 function Header() {
    const {systemThem, theme, setTheme} = useTheme();
    const [mounted, setMounted] = useState(false);
-  
+   const [searchInput, setSearchInput] = useState("");
+   const [startDate, setStartDate] = useState(new Date());
+   const [endDate, setEndDate] = useState(new Date());
+   const [numberOfGuest, setNumberOfGuest] = useState(1);
+   console.log(numberOfGuest)
+   //object
+   const selectionRanges = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection'
+   }
+   const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate)
+    setEndDate(ranges.selection.endDate)
+   }
+   
+   const resetInput = () => {
+     setSearchInput("");
+   }
+
+   console.log(searchInput)
+
+
   useEffect(() =>{
     setMounted(true);
   },[])
@@ -57,15 +83,17 @@ function Header() {
             src="https://links.papareact.com/qd3"
             layout='fill'
             objectFit='contain'
-            objectPosition="left"
+            objectPosition="left" 
         />
      </div>
      {/* Middle : Search */}
-      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm dark:border-slate-400'>
         <input  
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
         type="text" 
         className='flex-grow pl-5 bg-transparent outline-none
-        text-sm text-gray-600 placeholder-gray-400'
+        text-sm text-gray-600 placeholder-gray-400 dark:text-white dark:placeholder-slate-400'
         placeholder='Start your search'
          />
         <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white 
@@ -80,16 +108,51 @@ function Header() {
         {renderThemeChange()}
          </div>
          <div className='flex space-x-1'>
-          <p className='hidden md:inline-flex cursor-pointer'>
+          <p className='hidden lg:inline-flex cursor-pointer'>
            Become a Host
          </p>
          <GlobeAltIcon className='hidden md:inline-flex h-6 cursor-pointer'/>
        </div>
-      <div className='flex items-center  md:space-x-2 border-2 p-2 rounded-full'>
+      <div className='flex items-center  md:space-x-2 border-2 p-2 rounded-full dark:border-slate-400'>
          <MenuIcon className='h-6'/>
          <UserCircleIcon  className='h-6'/>
       </div>
      </div>
+     {searchInput && (
+      //this div will has the date range picker to install   
+      <div className='flex flex-col col-span-3 mx-auto dark:bg-white dark:text-black'>
+         <DateRangePicker 
+           ranges={[selectionRanges]}
+           minDate={new Date()}
+           rangeColors={["#FD5B61"]}
+           onChange={handleSelect}
+           className="mt-1 dark:bg-white dark:text-black"
+         />
+         <div className='flex items-center border-b mb-4  dark:bg-white dark:text-black'>
+            <h2 className='text-xl tracking-[1px] font-semibold flex-grow '>
+              Number of Guest
+            </h2>
+             <UsersIcon
+              className='h-5'
+             />
+             <input 
+              value={numberOfGuest}
+              onChange={(e) => setNumberOfGuest(e.target.value)}
+              type="number"
+              min={1}
+              className='w-12 pl-2 text-lg text-red-400 outline-none dark:bg-white'
+             />
+         </div>
+         <div className='flex'>
+                <button 
+                  className='flex-grow text-gray-500'
+                  onClick={resetInput}>
+                    Cancel
+                </button>
+                <button className='flex-grow text-red-400'>Search</button>
+         </div>
+      </div>
+      )}
 
     </header>
   )
